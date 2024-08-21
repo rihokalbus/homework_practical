f3b694add0133f9611b5b9ebc809c2a9af3d65df
riho.kalbus <riho.kalbus@voicecom.ee>
2024-08-21 20:58:08 +0300

    README and ping time measurement

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# homework_practical
+# Practical part of the homework
+## Description
+In this homework, a project with a dynamically extensible structure has been implemented. Assuming that the number
+of developers and modules under development will be small to medium, I decided to stick with one repository project with
+a flat structure.\
+All modules belonging to the project are in separate subdirectories. In addition, an include subdirectory is defined,
+which contains project-wide header files. A CMake macro, defined in the main directory's CMakeListtxt file, scans
+through the subdirectories of the project and automatically adds the directories in which CMakeListtxt exists.\
+As a demonstration, four subprojects have been implemented during the project - one library and three modules
+(drive, power pack and cooling) that implement simple ping pong over UDP according to the task description:
+* Drive will send out ping, cooling and powerpack will respond.
+* Drive will write to standard output:
+  * On receiving pong (source and measured ping time).
+  * On communication error or on timeout.
+* Cooling and powerpack will write to standard output:
+  * On receiving ping.
+## Building project
+In the main directory:
+```
+cmake .
+cmake --build .
+```
+## Installation
+```
+cmake --install . --prefix "/path/to/destination/directory"
+```
+## Running project
+```
+cd /path/to/destination/directory
+./cooling
+./powerpack
+./drive
+```
--- lib1/ping.c
+++ lib1/ping.c
@@ -38,15 +38,21 @@ static void comm_error(const int n, char *mod_name, char *fun_name) {
     printf("%s\n", szLog);
 }
 
-#define TV_NSEC_MAX 1000000000
+#define TV_NSEC_PER_SEC 1000000000
+#define TV_MS_PER_NS 0.000001
 
 // Set next alarm time for TIMER_ABSTIME, keeps tv_nsec under 1 second
 static void next_deadline(struct timespec *pdst) {
     pdst->tv_nsec += MONITOR_SLEEP * 1000000; // to nanoseconds
-    while (pdst->tv_nsec >= TV_NSEC_MAX) {
-        pdst->tv_nsec -= TV_NSEC_MAX;
+    while (pdst->tv_nsec >= TV_NSEC_PER_SEC) {
+        pdst->tv_nsec -= TV_NSEC_PER_SEC;
         pdst->tv_sec++;
     }
+}
+
+// Returns time difference beetween pstop and pstart in milliseconds (floating point format)
+static float time_diff(const struct timespec *pstop, const struct timespec *pstart) {
+    return (TV_NSEC_PER_SEC * (pstop->tv_sec - pstart->tv_sec) + pstop->tv_nsec - pstart->tv_nsec) * TV_MS_PER_NS;
 }
 
 void *pingThread(void *pArgs) {
@@ -54,6 +60,7 @@ void *pingThread(void *pArgs) {
     int *pResult = malloc(sizeof(int));
     char szLog[LOG_MAX_Z];
     char buffer[BUFSIZE_Z];
+    char timebuf[20];
     struct sockaddr_in srvAddr;
     const socklen_t srvAddrSize = sizeof(srvAddr);
 
@@ -72,14 +79,15 @@ void *pingThread(void *pArgs) {
     socklen_t len;
     const size_t pingLen = strlen(PING);
     bool first_cycle = true;
-    struct timespec sleep_to;
+    struct timespec sleep_to, ping_time, pong_time;
+    int rc;
 
     do {
         // do not woit on the first cycle
         if (first_cycle) {
             first_cycle = false;
         } else {
-            int rc = clock_gettime(CLOCK_MONOTONIC, &sleep_to);
+            rc = clock_gettime(CLOCK_MONOTONIC, &sleep_to);
             if (rc < 0) {
                 perror("clock_gettime failed");
                 *pResult = EXIT_FAILURE;
@@ -105,11 +113,19 @@ void *pingThread(void *pArgs) {
             comm_error(n, pA->szModuleName, "sendto");
             continue;
         }
+        // Start clock
+        rc = clock_gettime(CLOCK_MONOTONIC, &ping_time);
 
-        n = recvfrom(iSockFd, (char *)buffer, sizeof(buffer) - 1, MSG_WAITALL, (struct sockaddr *) &srvAddr, &len);
+        n = recvfrom(iSockFd, buffer, sizeof(buffer) - 1, MSG_WAITALL, (struct sockaddr *) &srvAddr, &len);
         if (n <= 0) {
             comm_error(n, pA->szModuleName, "recvfrom");
             continue;
+        }
+        // Stop clock and measure time
+        if( rc == 0 && clock_gettime(CLOCK_MONOTONIC, &pong_time) == 0) {
+            snprintf(timebuf, sizeof(timebuf) - 1, "%.4f", time_diff(&pong_time, &ping_time));
+        } else {
+            snprintf(timebuf, sizeof(timebuf) - 1, "-");
         }
         buffer[n] = '\0';
         // Remove trailing newline from buffer
@@ -119,10 +135,12 @@ void *pingThread(void *pArgs) {
         buffer[n] = '\0';
 
         if(strncmp(buffer, PONG, strlen(PONG)) == 0)
-            log_format(szLog, sizeof(szLog) - 1, INFO, pA->szModuleName, "%s", buffer);
+            log_format(
+                szLog, sizeof(szLog) - 1, INFO, pA->szModuleName, "%s (%s)ms", buffer, timebuf
+                );
         else
             log_format(
-                szLog, sizeof(szLog) - 1, ERROR, pA->szModuleName, "Unknown response: %s", buffer
+                szLog, sizeof(szLog) - 1, ERROR, pA->szModuleName, "Unknown response: %s (%s)ms", buffer, timebuf
                 );
         printf("%s\n", szLog);
     } while( *pA->pUp );
